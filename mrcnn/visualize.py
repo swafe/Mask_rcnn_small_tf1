32acd516bcbfa130d7b2ff6172e53da499779e33
swafe <svenfachinger@web.de>
2020-05-28 19:23:04 +0200

    change in vizualize to savefig

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -84,7 +84,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
                       figsize=(16, 16), ax=None,
                       show_mask=True, show_bbox=True,
-                      colors=None, captions=None):
+                      colors=None, captions=None, path_to_image=''):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [height, width, num_instances]
@@ -163,8 +163,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    patch_to_image = '/content/data/vgg_annotation/test/mask'+str(i)+'.jpg'
-    plt.savefig(patch_to_image, bbox_inches='tight', orientation= 'landscape') # pad_inches=-0.5,
+    plt.savefig(path_to_image, bbox_inches='tight', orientation= 'landscape') # pad_inches=-0.5,
     if auto_show:
         plt.show()
 
